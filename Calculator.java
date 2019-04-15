1632bcde37c0c4b0251d5708234bbf601eccdc0f
Raghav NS <35899273+IceCereal@users.noreply.github.com>
2019-04-15 13:08:09 +0530

    Add Buttons

--- Calculator.java
+++ Calculator.java
@@ -1,2 +1,27 @@
 import java.awt.*;
 import java.awt.event.*;
+
+class Calc implements ActionListener{
+	Frame f = new Frame();
+
+	Button num1 = new Button("1");
+	Button num2 = new Button("2");
+	Button num3 = new Button("3");
+	Button num4 = new Button("4");
+	Button num5 = new Button("5");
+	Button num6 = new Button("6");
+	Button num7 = new Button("7");
+	Button num8 = new Button("8");
+	Button num9 = new Button("9");
+	Button num0 = new Button("0");
+
+	Button sym_plus = new Button("+");
+	Button sym_minus = new Button("-");
+	Button sym_multiply = new Button("*");
+	Button sym_divide = new Button("/");
+	Button sym_eq = new Button("=");
+
+  public static void main(String[] args) {
+		new Calc();
+	}
+}
